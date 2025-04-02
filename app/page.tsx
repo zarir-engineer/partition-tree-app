3211242f722d6a25c4596b4826e66a8943314c7c
Zarir Engineer <zarir.utube@gmail.com>
2025-04-02 23:28:08 +0530

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ import CloudSpinnerGrid from '../components/CloudSpinnerGrid'; // Import the Clo
 const Page = () => {
   return (
     <div className="min-h-screen w-full flex flex-col items-center">
-      <h1 className="text-2xl mb-4">Hello, World!</h1>
+      <div className="absolute top-2 left-1/2 transform -translate-x-1/2 bg-white px-3 py-1 rounded shadow-md text-sm font-semibold">
+        0.125 = ⅛ portion
+      </div>
+      <h1 className="text-2xl mb-4">Vile Parle, Mumbai!</h1>
 
       {/* Ensure CloudSpinnerGrid takes full width */}
       <div className="w-full px-5">
